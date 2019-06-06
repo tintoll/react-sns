a0680c1c3e3b8dcfca303f1449508e9132d106f1
belselios@gmail.com <belselios@gmail.com>
2019-06-06 21:57:38 +0900

    댓글 작성 부분까지 완료

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -1,55 +1,51 @@
 import { all, fork, takeLatest, delay, put } from "redux-saga/effects";
-import { ADD_POST_REQUEST, ADD_POST_SUCCESS, 
-  ADD_POST_FAILURE, ADD_COMMENT_REQUEST, 
-  ADD_COMMENT_SUCCESS, ADD_COMMENT_FAILURE } from "../reducers/post";
+import {
+  ADD_POST_REQUEST,
+  ADD_POST_SUCCESS,
+  ADD_POST_FAILURE,
+  ADD_COMMENT_REQUEST,
+  ADD_COMMENT_SUCCESS,
+  ADD_COMMENT_FAILURE
+} from "../reducers/post";
 
 function* watchAddPost() {
   yield takeLatest(ADD_POST_REQUEST, addPost);
 }
-function* addPost(){
+function* addPost() {
   try {
     yield delay(2000);
-    console.log(dddd);
     yield put({
-      type : ADD_POST_SUCCESS,
-    })
-  } catch(e) {
+      type: ADD_POST_SUCCESS
+    });
+  } catch (e) {
     yield put({
-      type : ADD_POST_FAILURE,
-      error : e,
-    })
+      type: ADD_POST_FAILURE,
+      error: e
+    });
   }
 }
-function addPostAPI() {
-
-}
+function addPostAPI() {}
 
 function* watchAddComment() {
-  yield takeLatest(ADD_COMMENT_REQUEST, addComment)
+  yield takeLatest(ADD_COMMENT_REQUEST, addComment);
 }
-function* addComment(action){
+function* addComment(action) {
   try {
     yield delay(2000);
     yield put({
       type: ADD_COMMENT_SUCCESS,
       data: {
-        postId: action.data.postId,
-      },
-    })
+        postId: action.data.postId
+      }
+    });
   } catch (e) {
     yield put({
       type: ADD_COMMENT_FAILURE,
-      error: e,
-    })
+      error: e
+    });
   }
 }
-function addCommentAPI() {
-  
-}
+function addCommentAPI() {}
 export default function* postSaga() {
-  yield all([
-    fork(watchAddPost),
-    fork(watchAddComment)
-  ]);
-
-};
+  yield all([fork(watchAddPost), fork(watchAddComment)]);
+}
